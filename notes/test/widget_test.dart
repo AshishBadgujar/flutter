4d1033dfcae61ad983917bae08b911ef79255f98
Ashish Badgujar <abadgujar@nvidia.com>
2025-06-23 20:18:51 +0530

    temp

--- notes/test/widget_test.dart
+++ notes/test/widget_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_app/main.dart';
+import 'package:notes/main.dart';
 
 void main() {
   testWidgets('Notes app launches and shows default UI', (WidgetTester tester) async {
